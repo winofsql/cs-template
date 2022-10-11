9d997c1cb217054b71779c6993c08535f8e1e242
sworc <lightbox@nifty.com>
2022-10-12 08:27:02 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace cs_template
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      var curdir = Environment.CurrentDirectory;
+
+      Console.WriteLine( $"現在のディレクトリ : {curdir}" );
+
+    }
+  }
+}
